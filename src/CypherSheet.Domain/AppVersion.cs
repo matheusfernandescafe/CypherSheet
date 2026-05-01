aa0234a6f656e9163ee1f5218e7cbcf18f1f2e2a
Matheus Fernandes <matheusfernandesofc@gmail.com>
2026-05-01 20:13:18 -0300

    chore: v1.8.9

--- src/CypherSheet.Domain/AppVersion.cs
+++ src/CypherSheet.Domain/AppVersion.cs
@@ -4,7 +4,7 @@ namespace CypherSheet.Domain;
 
 public static class AppVersion
 {
-    public const string Version = "1.8.7";
+    public const string Version = "1.8.9";
     public static string BuildDate => GetBuildDate();
     public static string FullVersion => $"v{Version} ({BuildDate})";
     private static string GetBuildDate()
